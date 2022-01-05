05d363ffdff83b9668ebc14a601950eaff45208d
homubee <83688807+homubee@users.noreply.github.com>
2022-01-05 23:51:49 +0900

    init commit

---
+++ app/src/main/java/com/homubee/waterrate/view/InitializePublicActivity.kt
@@ -0,0 +1,12 @@
+package com.homubee.waterrate.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.homubee.waterrate.R
+
+class InitializePublicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initialize_public)
+    }
+}
---
+++ app/src/main/java/com/homubee/waterrate/view/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.homubee.waterrate.view
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.homubee.waterrate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalculate.isEnabled = false
+
+        binding.btnReset.setOnClickListener {
+            AlertDialog.Builder(this). run {
+                setTitle("주의")
+                setIcon(android.R.drawable.ic_dialog_alert)
+                setMessage("정말 초기화하시겠습니까?")
+                setPositiveButton("yes", DialogInterface.OnClickListener { p0, p1 ->
+                    Toast.makeText(applicationContext, "reset", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(applicationContext, InitializePublicActivity::class.java)
+                    startActivity(intent)
+                    binding.btnCalculate.isEnabled = true
+                })
+                setNegativeButton("no", null)
+                show()
+            }
+
+        }
+        binding.btnCalculate.setOnClickListener {
+            Toast.makeText(this, "calculate", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, CalculateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
