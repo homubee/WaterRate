3f2fb9aa437e1c40bcab62186f13fd838f538dbb
homubee <83688807+homubee@users.noreply.github.com>
2022-01-16 22:34:11 +0900

    #11 Add input form of this month count and usage

--- app/src/main/java/com/homubee/waterrate/view/CalculateActivity.kt
+++ app/src/main/java/com/homubee/waterrate/view/CalculateActivity.kt
@@ -1,15 +1,24 @@
 package com.homubee.waterrate.view
 
 import android.database.sqlite.SQLiteDatabase
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.text.InputType
+import android.util.TypedValue
+import android.view.Gravity
+import android.widget.EditText
+import android.widget.GridLayout
+import android.widget.TextView
+import androidx.core.view.setPadding
 import com.homubee.waterrate.databinding.ActivityCalculateBinding
 import com.homubee.waterrate.model.WaterRate
 import com.homubee.waterrate.util.DBHelper
 
 class CalculateActivity : AppCompatActivity() {
-    lateinit var adapter: WaterRateAdapter
+    companion object {
+        const val EDITTEXT_ID = 200
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +43,51 @@ class CalculateActivity : AppCompatActivity() {
             waterRateList.add(WaterRate(type, name, count, list))
         }
 
-        // 내용 확인용 임시 리사이클러뷰
-        adapter = WaterRateAdapter(1, waterRateList)
-        binding.recyclerData.layoutManager = LinearLayoutManager(this)
-        binding.recyclerData.adapter = adapter
+        for (i in waterRateList.indices) {
+            for (j in 1..3) {
+                val textView = if (j != 3) TextView(this) else EditText(this)
+                textView.gravity = Gravity.CENTER
+                textView.setPadding(Math.round(5*resources.displayMetrics.density))
+                textView.setBackgroundColor(Color.WHITE)
+
+                val glparams = GridLayout.LayoutParams()
+                glparams.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+
+                when(j) {
+                    1 -> {
+                        textView.text = waterRateList[i].name
+                        glparams.leftMargin = Math.round(1*resources.displayMetrics.density)
+                        glparams.rightMargin = Math.round(0.5*resources.displayMetrics.density).toInt()
+                    }
+                    2 -> {
+                        textView.text = waterRateList[i].lastMonthCount.toString()
+                        glparams.leftMargin = Math.round(0.5*resources.displayMetrics.density).toInt()
+                        glparams.rightMargin = Math.round(0.5*resources.displayMetrics.density).toInt()
+                    }
+                    3 -> {
+                        textView.id = EDITTEXT_ID + i
+                        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14f);
+                        textView.hint = "여기에 입력하세요"
+                        textView.inputType = InputType.TYPE_CLASS_NUMBER
+                        glparams.leftMargin = Math.round(0.5*resources.displayMetrics.density).toInt()
+                        glparams.rightMargin = Math.round(1*resources.displayMetrics.density)
+                    }
+                    else -> ""
+                }
+                glparams.topMargin = if (i == 0) {
+                    Math.round(0.5*resources.displayMetrics.density).toInt()
+                } else {
+                    Math.round(1*resources.displayMetrics.density)
+                }
+                glparams.bottomMargin = if (i == waterRateList.size-1) {
+                    Math.round(1*resources.displayMetrics.density)
+                } else {
+                    Math.round(0.5*resources.displayMetrics.density).toInt()
+                }
+
+                textView.layoutParams = glparams
+                binding.glTable.addView(textView)
+            }
+        }
     }
 }
