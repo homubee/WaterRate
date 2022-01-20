a258542061073ad4f64ea63f59e671a19557946a
homubee <83688807+homubee@users.noreply.github.com>
2022-01-21 00:15:07 +0900

    #11 Improve calculation algorithm (error handling system added)

--- app/src/main/java/com/homubee/waterrate/view/ResultActivity.kt
+++ app/src/main/java/com/homubee/waterrate/view/ResultActivity.kt
@@ -4,13 +4,21 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
 import android.view.Gravity
 import android.widget.GridLayout
 import android.widget.TextView
 import androidx.core.view.setPadding
 import com.homubee.waterrate.databinding.ActivityResultBinding
 import com.homubee.waterrate.model.WaterRate
+import kotlin.math.pow
 import kotlin.math.roundToInt
+
+// round function
+// Works as same as Excel
+fun roundDigit(number : Double, digits : Int): Double {
+    return (number / 10.0.pow(-digits)).roundToInt() * 10.0.pow(-digits)
+}
 
 class ResultActivity : AppCompatActivity() {
     lateinit var binding: ActivityResultBinding
@@ -27,16 +35,22 @@ class ResultActivity : AppCompatActivity() {
         val totalRate = intent.getIntExtra("totalRate", 0)
         val ratePerOne: Double = totalRate / totalUsage.toDouble()
 
+        // 사용량 리스트, 요금 리스트, 반올림 요금 차이 리스트
         val usageList = mutableListOf<Double>()
         val rateList = mutableListOf<Int>()
+        val diffRateList = mutableListOf<Int>()
 
-        // 사용량 리스트와 요금 리스트 초기화
+        // 상위 3개 리스트 초기화
         for (i in totalWaterRateList.indices) {
             usageList.add(thisMonthCountList[i] - totalWaterRateList[i].lastMonthCount)
-            rateList.add((ratePerOne*usageList[i]).roundToInt())
+            rateList.add(roundDigit(ratePerOne*usageList[i], -1).toInt())
+            diffRateList.add(roundDigit(ratePerOne*usageList[i], 0).toInt())
         }
 
         // 요금 데이터에 공용 수도 요금 분배
+        var rateSum = 0
+        var maxIndex = 0
+        var minIndex = 0
         for (i in totalWaterRateList.indices) {
             // 공용 수도일 경우 체크
             if (totalWaterRateList[i].type == 0) {
@@ -45,11 +59,31 @@ class ResultActivity : AppCompatActivity() {
                 for (j in totalWaterRateList[i].waterRateList.indices) {
                     for (k in totalWaterRateList.indices) {
                         if (totalWaterRateList[i].waterRateList[j] == totalWaterRateList[k].name) {
-                            rateList[k] = (rateList[k] + splitRate).roundToInt()
+                            rateList[k] = roundDigit(rateList[k] + splitRate, -1).toInt()
+                            diffRateList[k] = roundDigit(diffRateList[k] + splitRate, 0).toInt()
                         }
                     }
                 }
+            } else {
+                // 공용 수도 아닌 경우 인덱스 체크하며 실제값과 차이가 가장 큰 것과 작은 것 계산, 반올림 요금 차이 리스트도 여기서 완전히 확정됨
+                if (maxIndex == 0) {
+                    maxIndex = i
+                    minIndex = i
+                }
+                Log.d("rateList " + i.toString(), rateList[i].toString())
+                Log.d("diffRateList " + i.toString(), diffRateList[i].toString())
+                rateSum += rateList[i]
+                diffRateList[i] -= rateList[i]
+                maxIndex = if (diffRateList[i] < diffRateList[maxIndex]) { maxIndex } else { i }
+                minIndex = if (diffRateList[i] < diffRateList[minIndex]) { i } else { minIndex }
             }
+        }
+
+        // 실제 요금합과 청구된 요금 값이 차이가 있는 경우, 가장 차이가 큰 요금에서 차감, 가산
+        if (rateSum > totalRate) {
+            rateList[minIndex] += totalRate - rateSum
+        } else if (rateSum < totalRate) {
+            rateList[maxIndex] += totalRate - rateSum
         }
 
         // 표 내용 생성
