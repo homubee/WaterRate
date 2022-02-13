0d7cc889bbdbc6dfca7c7d2addf86c6fa0916af1
homubee <83688807+homubee@users.noreply.github.com>
2022-02-13 23:24:50 +0900

    #24 Add app info and open source license

---
+++ app/src/main/java/com/homubee/waterrate/view/AppInfoActivity.kt
@@ -0,0 +1,16 @@
+package com.homubee.waterrate.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.homubee.waterrate.databinding.ActivityAppInfoBinding
+
+class AppInfoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAppInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAppInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "앱 정보"
+    }
+}
